c34898c8e4ca55b7d84186ee1469f55ac09d807c
DESKTOP-CKJJILT\admin <pateldhruv123890@gmail.com>
2024-03-03 23:40:01 +0530

    create routing

--- 12-blogs/src/components/AuthLayout.jsx
+++ 12-blogs/src/components/AuthLayout.jsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-function AuthLayout({ children, authenticate = true }) {
+function AuthLayout({ children, authentication = true }) {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-    if (authenticate && authStatus !== authenticate) {
+    if (authentication && authStatus !== authentication) {
       navigate("/login");
-    } else if (!authenticate && authStatus !== authenticate) {
+    } else if (!authentication && authStatus !== authentication) {
       navigate("/");
     }
     setLoader(false);
-  }, [authStatus, authenticate, navigate]);
+  }, [authStatus, authentication, navigate]);
 
   return loader ? <div>.....Loading</div> : <>{children}</>;
 }
