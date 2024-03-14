63a1e2ddca3aa6c99767a3e061d1d0b558d9806b
DESKTOP-CKJJILT\admin <pateldhruv123890@gmail.com>
2024-03-15 00:20:05 +0530

    feat : design changes

--- api-call-in-react/src/pages/AddBlog.jsx
+++ api-call-in-react/src/pages/AddBlog.jsx
@@ -1,11 +1,5 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import Input from "../components/Input";
-import { ToastContainer } from "react-toastify";
-import { axiosTemplate, showToast } from "../common/helper";
-import { useNavigate } from "react-router-dom";
-import Button from "../components/Button";
-import BlogForm from "../components/BlogForm";
+import React from "react";
+import { BlogForm } from "../components";
 
 function AddBlog() {
   return (
